e894b56290cc7acc2d351d0450fc3e9ea86fb54c
Esa <esajuniarto52@gmail.com>
2023-07-11 11:08:52 +0700

    clearing API env

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,25 +1,26 @@
 import axios from "axios";
+import { baseURL } from "./endpoint";
 
 export default axios.create({
-  baseURL: process.env.REACT_APP_API_PROD,
+  baseURL1: process.env.REACT_APP_API_PROD,
 });
 
 export const getDataMemo = async () => {
-  const data = await axios.get("http://localhost:3030/DataMemo");
+  const data = await axios.get(`${baseURL}/DataMemo`);
   return data.data;
 };
 
 export const addPesan = async (body) => {
-  const save = await axios.post("http://localhost:3030/DataMemo", body);
+  const save = await axios.post(`${baseURL}/DataMemo`, body);
   return save;
 };
 
 export const getData = async () => {
-  const data = await axios.get("http://localhost:3030/dataProject");
+  const data = await axios.get(`${baseURL}/dataProject`);
   return data.data;
 };
 
 export const addData = async (body) => {
-  const save = await axios.post("http://localhost:3030/dataProject", body);
+  const save = await axios.post(`${baseURL}/dataProject`, body);
   return save;
 };
