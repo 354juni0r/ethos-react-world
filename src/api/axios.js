3645a2e09e579f300ef1cb02a41434eeab4e1aaf
354juni0r <esajuniarto52@gmail.com>
2023-07-12 10:43:16 +0700

    perbaikan merge axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,19 @@
 import axios from "axios";
 
+export default axios.create({
+  baseURL: process.env.REACT_APP_API_PROD,
+});
+
+export const getDataMemo = async () => {
+  const data = await axios.get("http://localhost:3030/DataMemo");
+  return data.data;
+};
+
+export const addPesan = async (body) => {
+  const save = await axios.post("http://localhost:3030/DataMemo", body);
+  return save;
+};
+
 export const deletePesan = async (id) => {
   return await axios.delete(`http://localhost:3030/DataMemo/${id}`);
 };
@@ -45,5 +59,5 @@ export const getUsers = async () => {
       },
     }
   );
-  return data.data;
+  return data.data;
 };
